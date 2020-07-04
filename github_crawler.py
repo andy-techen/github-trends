ea28d03c5c6ba939b1a7d462b70ca7d803f3462f
Dershan <andy27479@gmail.com>
2020-07-04 09:09:33 +0800

    added tokens

--- github_crawler.py
+++ github_crawler.py
@@ -16,13 +16,7 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 nltk.download('stopwords')
 nltk.download('punkt')
-
-# tokens
-t_consumer_key = "C4dNIgkl43788sKKZ7iNZZy3w"
-t_consumer_secret = "QBfEpERH8d6IdVCPBs4SQaLWfiXgG2b3KRGIV8QrJrBUNJWF2k"
-t_access_token = "712293623665045505-c2NWLT2AQqLVRRS8jcRKxyyHsQ1kpG6"
-t_access_secret = "o82dQvo1DEL2q4Cnp9EehhxsWUW8hs0huV5KwL2tX3dAs"
-g_access_token = "f6522f9ba517814474c80da6312ecb3347e341ec"
+from tokens import *
 
 # base variables
 keywords_ls = ["vpn", "anonymous browsing", "online tracking", "online surveillance"]
---
+++ tokens.py
@@ -0,0 +1,5 @@
+t_consumer_key = "C4dNIgkl43788sKKZ7iNZZy3w"
+t_consumer_secret = "QBfEpERH8d6IdVCPBs4SQaLWfiXgG2b3KRGIV8QrJrBUNJWF2k"
+t_access_token = "712293623665045505-c2NWLT2AQqLVRRS8jcRKxyyHsQ1kpG6"
+t_access_secret = "o82dQvo1DEL2q4Cnp9EehhxsWUW8hs0huV5KwL2tX3dAs"
+g_access_token = "f6522f9ba517814474c80da6312ecb3347e341ec"
