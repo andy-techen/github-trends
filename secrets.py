4dd24c6562932547e33fb76e21697707e948fa4e
Dershan <andy27479@gmail.com>
2020-07-17 00:36:44 +0800

    renewed token

--- secrets.py
+++ secrets.py
@@ -2,5 +2,5 @@ t_consumer_key = "C4dNIgkl43788sKKZ7iNZZy3w"
 t_consumer_secret = "QBfEpERH8d6IdVCPBs4SQaLWfiXgG2b3KRGIV8QrJrBUNJWF2k"
 t_access_token = "712293623665045505-c2NWLT2AQqLVRRS8jcRKxyyHsQ1kpG6"
 t_access_secret = "o82dQvo1DEL2q4Cnp9EehhxsWUW8hs0huV5KwL2tX3dAs"
-g_access_token = "82c30fd36c44a2f406a923a0b593b18d3c108cf6"
+g_access_token = "c2322967eda0cf3660868a7a096042a8963dc439"
 
