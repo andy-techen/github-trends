b9be618f38c7f647c762161298413ea9dfae199d
Dershan <andy27479@gmail.com>
2020-07-17 01:36:11 +0800

    renewed token

--- secrets.py
+++ secrets.py
@@ -2,5 +2,5 @@ t_consumer_key = "C4dNIgkl43788sKKZ7iNZZy3w"
 t_consumer_secret = "QBfEpERH8d6IdVCPBs4SQaLWfiXgG2b3KRGIV8QrJrBUNJWF2k"
 t_access_token = "712293623665045505-c2NWLT2AQqLVRRS8jcRKxyyHsQ1kpG6"
 t_access_secret = "o82dQvo1DEL2q4Cnp9EehhxsWUW8hs0huV5KwL2tX3dAs"
-g_access_token = "c2322967eda0cf3660868a7a096042a8963dc439"
+g_access_token = "86a9473d4efd6daf0e73ee33d13c090751403e03"
 
